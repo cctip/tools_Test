24b78b95a5b21195bc674fa4f40535a10b7c2add
hash512ecc <hash512ecc@protonmail.com>
2023-10-24 16:08:13 +0800

    fix webview open intent

--- android/app/src/main/java/com/hash/template/rnmodule/webview/CustomWebViewClient.kt
+++ android/app/src/main/java/com/hash/template/rnmodule/webview/CustomWebViewClient.kt
@@ -1,9 +1,11 @@
 package com.hash.template.rnmodule.webview
 
+import android.content.Intent
 import android.webkit.WebResourceRequest
 import android.webkit.WebResourceResponse
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import com.hash.template.utils.ToastUtils
 
 class CustomWebViewClient(val proxy: ClientProxy?) : WebViewClient() {
     override fun onPageFinished(view: WebView?, url: String?) {
@@ -25,5 +27,27 @@ class CustomWebViewClient(val proxy: ClientProxy?) : WebViewClient() {
             proxy?.onCanGoBack(canGoBack)
         }
         return res
+    }
+
+    override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+        val http = request?.url?.scheme?.startsWith("http") ?: false
+        return if (http) {
+            super.shouldOverrideUrlLoading(view, request)
+        } else {
+            try {
+                val intent = Intent(Intent.ACTION_VIEW).apply {
+                    data = request?.url
+                    addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                }
+                view?.context?.startActivity(intent)
+            } catch (e: Exception) {
+                //activity not found
+                e.printStackTrace()
+                view?.post {
+                    ToastUtils.show("No Application found!")
+                }
+            }
+            true
+        }
     }
 }
